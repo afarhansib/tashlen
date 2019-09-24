30839d8319427e71192627a962940dc6452c9834
afarhansib <afarhansib@gmail.com>
2019-09-25 04:05:43 +0700

    On branch master  Your branch is up to date with 'master/master'.
     Changes to be committed:
    	modified:   assets/css/main.css
    	new file:   assets/img/menu.svg
    	modified:   assets/js/main.js
    	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-	console.log("Hai Dunia!");
+console.log("Hai Dunia!");
+
+// Listen for orientation changes
+window.addEventListener("orientationchange", function() {
+	if (window.matchMedia("(orientation: portrait)").matches) {
+		// document.querySelector("#gantiOrientasi");
+		alert('tolong putar layar anda');
+	}
+	console.log(screen.orientation);
+}, false);
