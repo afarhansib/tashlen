a0d9c25af841ea5d44e62779dbead537e1ff73a4
afarhansib <afarhansib@gmail.com>
2019-09-27 16:10:22 +0700

    Commit selanjutnya, lihat sendiri lah diff nya
    Changes to be committed:
    	modified:   assets/css/main.css
    	deleted:    assets/fonts/OverpassMono-Bold/OverpassMono-Bold.eot
    	deleted:    assets/fonts/OverpassMono-Bold/OverpassMono-Bold.svg
    	deleted:    assets/fonts/OverpassMono-Bold/OverpassMono-Bold.ttf
    	deleted:    assets/fonts/OverpassMono-Bold/OverpassMono-Bold.woff
    	deleted:    assets/fonts/OverpassMono-Bold/preview.html
    	deleted:    assets/fonts/OverpassMono-Bold/styles.css
    	modified:   assets/js/main.js
    	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
-console.log("Hai Dunia!");
+// made with <3 by afarhansib | visit me on https://afarhansib.github.io/
+console.log('%c made with <3 by afarhansib', 'font-weight: bold; font-size: 30px;color: #063; text-shadow: 1px 1px 0px black , -1px -1px 0px white')
+console.log('%c visit me on https://afarhansib.github.io/', 'font-weight: bold; font-size: 20px;color: black; text-shadow: 1px 1px 0px #fc0 , -1px -1px 0px white')
 
-// Listen for orientation changes
+
+
+// variables
+const ovlRotScr = document.querySelector(".overlay-rotate-screen")
+
+
+
+// lihat perubahan rotasi
 window.addEventListener("orientationchange", function() {
 	if (window.matchMedia("(orientation: portrait)").matches) {
-		// document.querySelector("#gantiOrientasi");
-		alert('tolong putar layar anda');
-	}
-	console.log(screen.orientation);
-}, false);
+		ovlRotScr.setAttribute("data-visible", "true")
+    } else {
+        ovlRotScr.setAttribute("data-visible", "false")
+    }        
+}, false)
